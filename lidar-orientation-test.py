0f01e3bd361b39dc0653bcab139856b906c92958
Yuri Klebanov <yurikleb@gmail.com>
2022-03-22 13:58:15 +0900

    orientation tests

---
+++ lidar-orientation-test.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+image = np.zeros((1000,1000,3), np.uint8)
+window_name = 'image'
+
+# raw_pts = [ [100,100], [200,200], [300,300], [300,400], [400,300], [250,260], [100,100], [100, 150], [240,280] ]
+points =  np.random.randint(10,500, size=(8,2))
+# points =  np.array(raw_pts)
+for point in points:
+    image = cv2.circle(image, point, 6, (255,255,255), 1)
+
+ellipse = cv2.fitEllipse(points)
+image = cv2.ellipse(image, ellipse, (0,255,0), 2)
+
+
+cv2.imshow(window_name, image)  
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
